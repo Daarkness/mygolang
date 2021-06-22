8e68f6be93ea34aec56e8466d8b6eb18527d7413
apple <apple@appledeMacBook-Pro.local>
2021-06-22 12:04:21 +0800

    add doc

--- test.go
+++ test.go
@@ -1,5 +1,8 @@
 package mygolang
 
+// At completes the following operation of Set(). It defines posttion of value in Set() and return the new value set.
+//
+// The usage of At() is perhaps the most important. This function will recursivly search for child value, and set the new value specified by Set() or SetXxx() series functions. Please unfold and read the following examples, they are important.
 func Add(a int, b int) int {
 	return (a + b)
 }
